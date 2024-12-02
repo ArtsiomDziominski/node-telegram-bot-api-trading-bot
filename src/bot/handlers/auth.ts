8254996e4c70665c14845a5e4d0b9e2e2a51e528
ArtemDeminski <artem.d@konomic.com>
2024-12-02 23:01:41 +0400

    add login

---
+++ src/bot/handlers/auth.ts
@@ -0,0 +1,47 @@
+import TelegramBot from 'node-telegram-bot-api';
+import axios from "axios";
+import { encryptPassword } from "../../utils/encrypt";
+import WebSocketClient from "../services/websocketClient";
+
+
+export const handleAuth = (bot: TelegramBot) => {
+	const wsClient = WebSocketClient.getInstance(bot);
+	const BURL = process.env.SERVER_URL;
+	const userLoginRequest: Record<string, any> = {};
+
+	bot.onText(/\/login/, (msg: any) => {
+		userLoginRequest[msg.chat.id] = {isLogin: true, isPassword: false};
+		bot.sendMessage(msg.chat.id, 'Введите логин:');
+	});
+
+	bot.on('message', (msg: any) => {
+		const chatId = msg.chat.id.toString();
+		const text = msg?.text;
+		if (userLoginRequest?.[chatId]?.isLogin) {
+			userLoginRequest[chatId] = {...userLoginRequest[chatId], isLogin: false, mail: text, isPassword: true}
+			bot.sendMessage(chatId, 'Введите пароль:');
+		} else if (userLoginRequest?.[chatId]?.isPassword) {
+			userLoginRequest[chatId].isPassword = false;
+			const passwordEncrypt = encryptPassword(text);
+			axios.post(BURL + '/auth/login',
+				{
+					mail: userLoginRequest[chatId].mail,
+					password: passwordEncrypt
+				}).then((response: any) => {
+				const token = response?.data?.token
+				if (token) {
+					wsClient.sendMessage(chatId, {authorization: token});
+					bot.sendMessage(chatId, 'Вы успешно вошли');
+				} else bot.sendMessage(chatId, 'Не удалось получить токен');
+			}).catch((error: any) => {
+				bot.sendMessage(chatId, error?.response?.data?.message || 'Не удалось выполнить вход');
+			})
+			delete userLoginRequest[chatId];
+		}
+	});
+
+	bot.onText(/\/logout/, (msg: any) => {
+		wsClient.removeUser(msg.chat.id);
+		bot.sendMessage(msg.chat.id, 'Вы успешно вышли. Для входа используйте команду /login');
+	});
+};
--- src/bot/handlers/commands.ts
+++ src/bot/handlers/commands.ts
@@ -3,6 +3,10 @@ import TelegramBot from 'node-telegram-bot-api';
 export const handleCommands = (bot: TelegramBot) => {
 
 	bot.onText(/\/start/, (msg: any) => {
-		bot.sendMessage(msg.chat.id, 'Привет!');
+		bot.sendMessage(msg.chat.id, 'Для входа используйте команду /login');
+	});
+
+	bot.onText(/\/info/, (msg: any) => {
+		bot.sendMessage(msg.chat.id, 'Этот бот предназначен для отправки уведомлений о ваших ботах');
 	});
 };
