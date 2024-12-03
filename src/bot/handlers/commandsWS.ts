d2e8b9be5b2951386dd4e45196174cecaf33fbd0
ArtemDeminski <artem.d@konomic.com>
2024-12-03 10:50:23 +0400

    add reconnect ws

--- src/bot/handlers/commandsWS.ts
+++ src/bot/handlers/commandsWS.ts
@@ -1,6 +1,21 @@
 import TelegramBot from 'node-telegram-bot-api';
 import WebSocketClient from "../services/websocketClient";
 
+enum WebSocketStatus {
+	'CONNECTING',
+	'OPEN',
+	'CLOSING',
+	'CLOSED'
+}
+
+const WebSocketStatusInfo = {
+	[WebSocketStatus.CONNECTING]: 'Устанавливается соединение...',
+	[WebSocketStatus.OPEN]: 'Соединение активно и стабильное',
+	[WebSocketStatus.CLOSING]: 'Соединение закрывается...',
+	[WebSocketStatus.CLOSED]: 'Соединение закрыто',
+};
+
+
 export const handleCommandsWS = (bot: TelegramBot) => {
 	const wsClient = WebSocketClient.getInstance(bot);
 
@@ -12,10 +27,10 @@ export const handleCommandsWS = (bot: TelegramBot) => {
 		bot.sendMessage(chatId, 'Вы отключены от WebSocket.');
 	});
 
-	bot.onText(/\/checkstatusconnect/, (msg: any) => {
+	bot.onText(/\/check/, (msg: any) => {
 		const chatId = msg.chat.id.toString();
-		const status = wsClient.getStatus(chatId);
-		bot.sendMessage(chatId, `Статус WebSocket ${status}`);
+		const status = wsClient.getStatus(chatId) as WebSocketStatus || undefined;
+		bot.sendMessage(chatId, WebSocketStatusInfo?.[status || WebSocketStatus.CLOSED]);
 	});
 
 	bot.onText(/\/send (.+)/, (msg: any, match: any) => {
